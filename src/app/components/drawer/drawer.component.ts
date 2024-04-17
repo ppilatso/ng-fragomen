2ce1e9621ac4b692142659272a74dca6f2482a6c
Phenyo Pilatso <pilatsophenyo@gmail.com>
2024-04-17 21:56:56 +0200

    add navigation-bar and sidebar

---
+++ src/app/components/drawer/drawer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-drawer',
+  standalone: true,
+  imports: [],
+  templateUrl: './drawer.component.html'
+})
+export class DrawerComponent {
+
+}
